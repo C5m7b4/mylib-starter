f29ec36f0fb329b909c7d43f50ad2f0a66b9ee54
C5m7b4 <mbedingfield@dcrpos.com>
2022-09-25 21:48:10 -0500

    Api Extractor basic setup

---
+++ src/subtract.ts
@@ -0,0 +1,11 @@
+/**
+ * Calculate the difference between two numbers
+ * @param a - first number
+ * @param b - second number
+ * @returns
+ *
+ * @beta
+ */
+export function subtract(a: number, b = 0): number {
+  return a - b;
+}
